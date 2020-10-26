5a2f13822907677d68175be0038fededf46bce02
Colin Szeto <kolinszeto@gmail.com>
2020-10-26 18:04:27 +0000

    Connected the group website to the github repository. Implemented Jinja for the nav bar at the top

---
+++ README.md
@@ -0,0 +1,15 @@
+### Usage of different materials
+
+GitHub repository - https://github.com/kylem314/ChessOO - implemented previous code onto github
+
+Example of coding with IntelliJ - https://photos.app.goo.gl/d3184E2keVatUb68A - implemented intellij development on previous project
+
+Show usage of Flask and routes - see in "main.py" https://repl.it/@colinszeto/Group-Portfolio#main.py
+
+Bootstrap usage - home.html, line 120
+
+Show usage of Jinja - see in "base.html" within the templates folder: https://repl.it/@colinszeto/Group-Portfolio#templates/base.html
+
+Show your project Theme - Chess Chess Chess, see the background of the home page as well as the second tab on the nav bar "Chess with JS"
+
+Show each individual having a code checkin and comment
--- main.py
+++ main.py
@@ -3,24 +3,22 @@ from flask import Flask, render_template
 #create a Flask instance
 app = Flask(__name__)
 
-# Data setup (Listy)
-ProjectName = "Project Name"
-ProjectPlan = "Project Plan"
-Repl = "Repl"
-Resources = "Resources"
 InfoDb = []
-# First Project
-InfoDb.append({ProjectName: "Hello Series Project", ProjectPlan: "http://nighthawkcoders.cf/courses/python/", Repl: "https://repl.it/@jmort1021/Python-Hello-Series#README.mn", Resources: "https://padlet.com/jmortensen7/csp2021" })
-# Second Project
-InfoDb.append({ProjectName: "Flask Project", ProjectPlan: "http://nighthawkcoders.cf/courses/python/", Repl: "https://repl.it/@jmort1021/Web-Portfolio-Series#main.py", Resources: "https://padlet.com/jmortensen7/csptime1_2" })
-InfoDb.append({ProjectName: "Future Project", ProjectPlan: "http://nighthawkcoders.cf/courses/python/", Repl: "https://repl.it/@jmort1021/Web-Portfolio-Series#main.py", Resources: "https://padlet.com/jmortensen7/csptime1_2" })
 
 #connects default URL of server to a python function
 @app.route('/')
 def home():
     #function uses Flask import (Jinga) to render HTML
     #data is passed as a parameter
-    return render_template("home.html", data=InfoDb)
+    return render_template("home.html", data=InfoDb)#home has to be under templates
+
+@app.route("/chessJs")#for the dragable chess file
+def chessJS():
+  return render_template("chessJs.html")
+
+@app.route("/journals")#for storing all the links to the webpage
+def journals():
+  return render_template("journals.html",repl="link to repl of website", website ="link to personal website")#allows to define the text that is hyperlinked on the the personal journals
 
 if __name__ == "__main__":
     #runs the application on the repl development server
