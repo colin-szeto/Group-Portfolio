3165fe17ae9dbee3963fd0e602eeae58dd9d4952
DESKTOP-TBBDDJ1\Cyber <kolinszeto@gmail.com>
2020-10-30 01:06:22 -0700

    able to install flask by hvoering over the flask error and remove the hositng address from main.py

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ from flask import Flask, render_template
 #create a Flask instance
 app = Flask(__name__)
 
-InfoDb = []
+#InfoDb = []
 
 #connects default URL of server to a python function
 @app.route('/')
 def home():
     #function uses Flask import (Jinga) to render HTML
     #data is passed as a parameter
-    return render_template("home.html", data=InfoDb)#home has to be under templates
+    return render_template("home.html")#home has to be under templates
 
 @app.route("/chessJs")#for the dragable chess file
 def chessJS():
+
   return render_template("chessJs.html")
 
 @app.route("/journals")#for storing all the links to the webpage
 def journals():
-  return render_template("journals.html",repl="link to repl of website", website ="link to personal website")#allows to define the text that is hyperlinked on the the personal journals
+  return render_template("journals.html",repl="repl of website", website ="link to personal website")#allows to define the text that is hyperlinked on the the personal journals
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True, port='3000', host='0.0.0.0')
+    app.run(debug=True)
